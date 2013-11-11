7db0f4a7590938a666f00f8fad26121b583993ea
Evgeny Rusanov <bartl@macsoftex.com>
2013-11-11 11:43:40 +0300

    Initial Commit

---
+++ SlidshowViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidshowViewController
+//
+//  Created by Evgeny Rusanov on 11.11.13.
+//  Copyright (c) 2013 Evgeny Rusanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlidshowViewControllerTests/SlidshowViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  SlidshowViewControllerTests.m
+//  SlidshowViewControllerTests
+//
+//  Created by Evgeny Rusanov on 11.11.13.
+//  Copyright (c) 2013 Evgeny Rusanov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SlidshowViewControllerTests : XCTestCase
+
+@end
+
+@implementation SlidshowViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
